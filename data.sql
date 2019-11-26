20e4b857e08e2c838dbcac022f86ec5c18c21e62
NgocNam3518 <ngocnam3518@gmail.com>
2019-11-27 02:31:09 +0700

    add exam, diem, quest

--- data.sql
+++ data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 17, 2019 lúc 02:04 PM
+-- Thời gian đã tạo: Th10 26, 2019 lúc 08:30 PM
 -- Phiên bản máy phục vụ: 10.4.6-MariaDB
 -- Phiên bản PHP: 7.3.9
 
@@ -19,8 +19,47 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `data`
+-- Cơ sở dữ liệu: `laravel`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `diem`
+--
+
+CREATE TABLE `diem` (
+  `user_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `diem` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `diem`
+--
+
+INSERT INTO `diem` (`user_id`, `exam_id`, `diem`) VALUES
+(1, 1, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `exam`
+--
+
+CREATE TABLE `exam` (
+  `exam_id` bigint(20) UNSIGNED NOT NULL,
+  `exam_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exam_describe` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `exam`
+--
+
+INSERT INTO `exam` (`exam_id`, `exam_name`, `exam_describe`, `id`) VALUES
+(1, 'kiểm tra cuối kỳ', 'kiểm tra kiến thức toán THPT', 4);
 
 -- --------------------------------------------------------
 
@@ -73,6 +112,27 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `quest`
+--
+
+CREATE TABLE `quest` (
+  `quest_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `quest` varchar(11) NOT NULL,
+  `answer` varchar(11) NOT NULL,
+  `rightAnswer` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `quest`
+--
+
+INSERT INTO `quest` (`quest_id`, `exam_id`, `quest`, `answer`, `rightAnswer`) VALUES
+(1, 1, 'abc?', 'yes,no', 'yes');
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `users`
 --
 
@@ -93,12 +153,21 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'admin', 'admin', NULL, '21232f297a57a5a743894a0e4a801fc3', NULL, NULL, NULL),
-(2, 'asimo', 'asimo@gmail.com', NULL, '$2y$10$pPL/kQ5dxZxOpSIARwM4reUiDMcxIwG17u8pJCFdQy8BikDQd6dV6', 'huIQVpvqulh2iPkWcLpFdP05liqeAF3WVS3q8u526K5usthRPObO5iu4AjXT', '2019-11-12 19:42:49', '2019-11-12 19:42:49'),
-(3, 'user', 'user@gmail.com', NULL, '$2y$10$gLmxHi7qUXrvrlwoglWmE.Bqv9QnvwmVY3fV9jIMwube3oahV6Juy', NULL, '2019-11-14 09:00:31', '2019-11-14 09:00:31');
+(2, 'asimo', 'asimo@gmail.com', NULL, '$2y$10$pPL/kQ5dxZxOpSIARwM4reUiDMcxIwG17u8pJCFdQy8BikDQd6dV6', 'cPIshDiOGIK3HrBPl3GV40gKveZbrRkGefPFJgp0riD2rZCPyNuG1lvfcgP9', '2019-11-12 19:42:49', '2019-11-12 19:42:49'),
+(3, 'user', 'user@gmail.com', NULL, '$2y$10$gLmxHi7qUXrvrlwoglWmE.Bqv9QnvwmVY3fV9jIMwube3oahV6Juy', NULL, '2019-11-14 09:00:31', '2019-11-14 09:00:31'),
+(4, 'test', 'test@gmail.com', NULL, '$2y$10$DFUmUV4p2nHgmewMqyQ2iuPSVIuktA9sBXuWMetuboCZNYQPp.Jvq', NULL, '2019-11-21 06:01:20', '2019-11-21 06:01:20'),
+(5, 'Nam', 'nam@gmail.com', NULL, '$2y$10$g8a2kre2a//b8akpoM7g4eg4G/Y.KR5Jft3soQoQfe.XptDLSyrBu', NULL, '2019-11-26 10:48:43', '2019-11-26 10:48:43');
 
 --
 -- Chỉ mục cho các bảng đã đổ
 --
+
+--
+-- Chỉ mục cho bảng `exam`
+--
+ALTER TABLE `exam`
+  ADD PRIMARY KEY (`exam_id`),
+  ADD UNIQUE KEY `exam_name` (`exam_name`);
 
 --
 -- Chỉ mục cho bảng `failed_jobs`
@@ -130,6 +199,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT cho bảng `exam`
+--
+ALTER TABLE `exam`
+  MODIFY `exam_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT cho bảng `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -145,7 +220,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
